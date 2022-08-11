bb90cb42b662ab2bb6e1cffaa57ca0e2816bcdd2
uesleiregis <uesleiregis@live.com>
2022-08-11 18:03:04 -0300

    comit hooks

---
+++ aula-hooks/src/Components/App/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Counter from '../Counter/Counter'
+import SmartCounter from '../Smartcounter/Smartcounter';
+
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      {/* <Counter/> */}
+
+      <SmartCounter/>
+    </>
+  );
+}
+
+export default App;
