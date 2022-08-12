a18103f43142f76bd4e39a01483095edc0561865
uesleiregis <uesleiregis@live.com>
2022-08-12 14:51:45 -0300

    contadorifood atualizado

--- aula-hooks/src/Components/Contadorifood/Contadorifood.js
+++ aula-hooks/src/Components/Contadorifood/Contadorifood.js
@@ -6,7 +6,14 @@ export default function ContadorIfood(){
     
 
     const [value, setValue] = useState(1);
+
+    //const [buttonStyle, setButtonStyle] = useState("counter-button-minus-active");
  
+
+    useEffects(()=>{
+        document.getElementById("moeda").innerHTML = 2*value;
+    },[value])
+    // toda vez que o value for alterado, o comando dentro
 
    function up(){
         setValue(value+1)
